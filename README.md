d6d85a157e3b0acf0d8144b624aef3fab202d781
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2023-04-10 09:53:14 +0200

    Change installer from .exe to .msi (WINDOWS ONLY)

--- README.md
+++ README.md
@@ -10,21 +10,19 @@ DK-C++ is an IDE for beginners with simple commands:
 
 ## How to install
 1. *(Strongly recommended, but optional)* Deactivate your antivirus.
-2. Open `DK-C++.Setup.exe`.
-3. If it closes and doesn't open DK-C++, do Step 2 again.
+2. Open `DK-C++.Setup.msi`.
+3. Complete the installation.
 4. *(Also strongly recommended, but again, optional)* Activate your antivirus.
 
 ## How to compile it yourself
+**NOTE**: This is only for Windows. I don't plan building this app to exist on Linux/Mac.
 1. Have [Node.js](https://nodejs.org) and [NPM](https://npmjs.com) installed.
 2. Download `Source code (zip)` from [the release](https://github.com/EntityPlantt/DK-CPP/releases).
 3. Extract the folder.
 4. Go with Command Prompt or PowerShell in the extracted folder.
 5. Run the set below in the command prompt:
    ```batch
-   npm i -D electron
-   npm i -D electron-packager
-   npm i -D electron-winstaller
-   npm run package
-   npm run installer
+   npm i -D
+   npm run build
    ```
-6. Your build is in `installer/Setup.exe`.
+6. Your build is in the `installer` folder.
--- installer.js
+++ installer.js
@@ -1,21 +1,28 @@
-const { createWindowsInstaller } = require("./node_modules/electron-winstaller/lib/index");
-const { join } = require("path");
-
 (async () => {
-	console.log("Creating installer...");
-	try {
-		await createWindowsInstaller({
-			appDirectory: join(__dirname, "DK-C++-win32-x64"),
-			outputDirectory: join(__dirname, "installer"),
-			exe: "DK-C++.exe",
-			iconUrl: join(__dirname, "icon.ico"),
-			setupIcon: join(__dirname, "icon.ico"),
-			setupExe: "DK-C++ Setup.exe",
-			setupMsi: "DK-C++ Windows Setup.msi"
-		});
-		console.log("Created MSI installer");
-	}
-	catch (e) {
-		console.error(e);
-	}
+    const { MSICreator } = require("electron-wix-msi"), { join } = require("path"), package = require(join(__dirname, "package.json"));
+    console.log("0%   | Starting...");
+    const msiCreator = new MSICreator({
+        appDirectory: join(__dirname, "DK-C++-win32-x64"),
+        outputDirectory: join(__dirname, "installer"),
+        exe: "DK-C++",
+        description: package.description,
+        version: package.version,
+        name: "DK-C++",
+        icon: "icon.ico",
+        manufacturer: package.author.name,
+        shortName: package.name,
+        upgradeCode: "1fb1dd48-5369-46f4-b18d-35c0ec62ab68",
+        arch: "x64",
+        features: { autoUpdate: true },
+        ui: {
+            enabled: true,
+            chooseDirectory: true,
+            images: { background: join(__dirname, "resources", "installer-background.jpg") }
+        }
+    });
+    console.log("25%  | Created MSICreator");
+    const wxs = await msiCreator.create();
+    console.log("50%  | Created .wxs template file");
+    await msiCreator.compile();
+    console.log("100% | Done!");
 })();
